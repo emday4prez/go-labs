ca6a702353cba9627f1d4a1ca6598f6622f8f6b5
Emerson <emerson.day@gmail.com>
2025-08-10 08:44:55 -0700

    use flags

--- gocount/main.go
+++ gocount/main.go
@@ -1,25 +1,69 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"flag"
 	"fmt"
+	"io"
 	"os"
 )
 
 func main() {
 
-	if len(os.Args) < 2 {
-		fmt.Println("Error: No filename provided.")
-		fmt.Println("Usage: go run main.go <filename>")
-		os.Exit(1)
-	}
-	filename := os.Args[1]
+	linesFlag := flag.Bool("l", false, "Count lines")
+	wordsFlag := flag.Bool("w", false, "Count words")
+	bytesFlag := flag.Bool("c", false, "Count bytes")
+	flag.Parse()
 
-	content, err := os.ReadFile(filename)
-	if err != nil {
-		fmt.Println("Error: cannot read file")
-		os.Exit(1)
+	var reader io.Reader
+
+	if len(flag.Args()) > 0 {
+		filename := flag.Args()[0]
+		file, err := os.Open(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "gocount: error opening file: %v\n", err)
+			os.Exit(1)
+		}
+		// IMPORTANT: Ensure the file is closed when main exits.
+		defer file.Close()
+		reader = file
+	} else {
+		reader = os.Stdin
 	}
 
-	fmt.Print(string(content))
+	lines, words, bytes := count(reader)
+
+	noFlagsSet := !*linesFlag && !*wordsFlag && !*bytesFlag
+
+	if *bytesFlag || noFlagsSet {
+		fmt.Printf("Bytes: %d\n", bytes)
+	}
+	if *wordsFlag || noFlagsSet {
+		fmt.Printf("Words: %d\n", words)
+	}
+	if *linesFlag || noFlagsSet {
+		fmt.Printf("Lines: %d\n", lines)
+	}
+}
+
+func count(r io.Reader) (int, int, int) {
+	scanner := bufio.NewScanner(r)
+
+	lines := 0
+	words := 0
+	byteCount := 0
+
+	for scanner.Scan() {
+		lines++
+
+		lineBytes := scanner.Bytes()
+		byteCount += len(lineBytes)
+		words += len(bytes.Fields(lineBytes))
+
+	}
+
+	byteCount += lines
+	return lines, words, byteCount
 
 }
